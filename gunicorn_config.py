1d0faeea974202a5cc0c9573196d6b4ba21b9168
BAPNuSigma <bapfdu@gmail.com>
2025-04-14 13:07:31 -0400

    Update gunicorn_config.py
    Fixed Exited Status 1 Error

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,14 +1,17 @@
 import os
 import sys
+import multiprocessing
 
 # Add the current directory to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Gunicorn configuration
-bind = "0.0.0.0:8000"
-workers = 4
+bind = "0.0.0.0:" + str(int(os.environ.get("PORT", 5000)))
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "gthread"
+threads = 2
 timeout = 120
-worker_class = "sync"
+keepalive = 5
 accesslog = "-"
 errorlog = "-"
 loglevel = "info" 
